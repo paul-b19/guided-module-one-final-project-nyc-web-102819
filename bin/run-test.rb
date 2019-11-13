2c0dbabb04357a3f478fcd37aaae3d939ea9a5df
Pavel Beletsky <pavel.beletsky@live.com>
2019-11-13 09:29:15 -0500

    Added design, refactored poem_title, added lessons logic, refactored menus

--- bin/run-test.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../config/environment'
-
-def start
-    puts "you ran!"
-end
-binding.pry
-
-start
--- lib/lesson.rb
+++ lib/lesson.rb
@@ -1,4 +1,19 @@
 class Lesson < ActiveRecord::Base
+
+    @@prompt = TTY::Prompt.new
+
     belongs_to :user
     belongs_to :poem
+
+    def self.lesson_creation(arg)
+        Lesson.create(arg)
+    end
+
+    def self.lesson_selection
+        l = Lesson.where(user_id: User.user_id).map do |lesson|
+            Poem.find_by(id: lesson.poem_id).title
+        end   
+        @@selected_lesson = @@prompt.select("Select Poem:", l) 
+        Poem.poem_title({title: @@selected_lesson}) 
+    end
 end
