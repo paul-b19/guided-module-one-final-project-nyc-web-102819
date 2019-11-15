633a9401e072b115acbeb313f3b409ae89627131
Pavel Beletsky <pavel.beletsky@live.com>
2019-11-15 11:06:03 -0500

    final changes to logic and beautifying app

--- lib/author.rb
+++ lib/author.rb
@@ -7,26 +7,32 @@ class Author < ActiveRecord::Base
 
     def self.search_by_author
         CommandLineInterface.logo("./design/logo_small.png", false)
-        @@author_search = @@prompt.ask("Please enter the author you are looking for:"){ 
-            |input| input.validate /^^(?=.{1,40}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._ ]+(?<![_.])$/, 
-            "Sorry, your author entry must be at least 1 character and not contain special symbols." }
+        @@author_search = User.validate("ask", "author")
         if Author.find_by("name like ?", "%#{@@author_search}%")
-            p = Author.where("name like ?", "%#{@@author_search}%").map {|author| author.name}.sort
-
-            @@selected_author_name = @@prompt.select("Select Author:", p)
-            @@selected_author = Author.find_by(name: "#{@@selected_author_name}")
-            @@author_poems = Poem.where(author: @@selected_author).map {|poem| poem.title}.sort
-
-            @@selected_title = @@prompt.select("Select Poem", @@author_poems)
-            Poem.poem_title({title: @@selected_title})
+            display_author_names_and_select
         else
-            puts "Sorry, author was not found. Please try another input."
-            @@menu_selection_footer = @@prompt.select("Options:") do |menu|
-                menu.choice '* Try Again', -> {search_by_author}
-                menu.choice "* Back to Main Menu", -> {
-                CommandLineInterface.logo("./design/logo_small.png", false);
-                CommandLineInterface.general_menu}
-            end
+            author_name_not_found
+        end
+    end
+
+    def self.display_author_names_and_select
+        p = Author.where("name like ?", "%#{@@author_search}%").map {|author| author.name}.sort
+
+        @@selected_author_name = @@prompt.select("Select Author:", p)
+        @@selected_author = Author.find_by(name: "#{@@selected_author_name}")
+        @@author_poems = Poem.where(author: @@selected_author).map {|poem| poem.title}.sort
+
+        @@selected_title = @@prompt.select("Select Poem", @@author_poems)
+        Poem.poem_title({title: @@selected_title})
+    end
+
+    def self.author_name_not_found
+        puts "Sorry, author was not found. Please try another input."
+        @@menu_selection_footer = @@prompt.select("Options:") do |menu|
+            menu.choice '⏪ Try Again', -> {search_by_author}
+            menu.choice "🔍 Poem Search", -> {
+            CommandLineInterface.logo("./design/logo_small.png", false);
+            Poem.search_for_poem}
         end
     end
 
